2e995f4db436376e7e2614622429e54df5991f28
Ignacio Baclini <ignaciobaclini@hotmail.com>
2022-11-15 04:08:28 -0300

    coloco las cards en el html con el appendTo

--- js/card.js
+++ js/card.js
@@ -3,10 +3,10 @@ function Card(heading, description, image) {
     this.description = description
     this.image = image
     
-    this.appendTO = function () {
+    this.appendTo = function (destinationElement) {
 
         let card = document.createElement('a')
-        card.classList('card')
+        card.classList.add('card')
         card.href = 'https://sony.com'
 
     card.innerHTML = `<article class="card__article">
@@ -25,6 +25,8 @@ function Card(heading, description, image) {
                 </div>
             </article>`
 
+
+            destinationElement.appendChild(card)
     }
 
 
@@ -37,15 +39,92 @@ console.log (elemCardsContainer)
 
 
 const card1 = new Card(
-            'tv',
-            'lorem insum....',
-            'imagen del celular',
+            'celular',
+            'iphone 7',
+            'img/productos/iphone-x.jpg',
     )
 
     const card2 = new Card(
-        'tv',
-        'lorem insum....',
+        'celular',
+        'iphone 8',
+        'img/productos/iphone-xr.jpg',
+    )
+
+    const card3 = new Card(
+        'celular',
+        'iphone 9',
+        'img/productos/iphone1.jpg',
+    )
+
+    const card4 = new Card(
+        'celular',
+        'iphone X',
+        'img/productos/iphone2.png',
+    )
+
+    const card5 = new Card(
+        'celular',
+        'iphone 11 pro',
+        'img/productos/iphone-11-pro.jpg',
+    )
+
+    const card6 = new Card(
+        'celular',
+        'iphone 12',
+        'img/productos/iphone-12.jpg',
+    )
+
+    const card7 = new Card(
+        'celular',
+        'iphone 13',
+        'img/productos/iphone-13.jpg',
+    )
+
+    const card8 = new Card(
+        'celular',
+        'iphone 14',
+        'img/productos/iphone3.jpg',
+    )
+
+    const card9 = new Card(
+        'celular',
+        'iphone 14 pro',
         'imagen del celular',
-)
+    )
+
+
+
     console.log(card1)
     console.log(card2)
+    console.log(card3)
+    console.log(card4)
+    console.log(card5)
+    console.log(card6)
+    console.log(card7)
+    console.log(card8)
+    console.log(card9)
+    
+
+    const cards = [
+        card1,
+        card2,
+        card3,
+        card4,
+        card5,
+        card6,
+        card7,
+        card8,
+        card9,
+    ]
+
+    console.log(cards)
+
+    card1.appendTo(elemCardsContainer)
+    card2.appendTo(elemCardsContainer)
+    card3.appendTo(elemCardsContainer)
+    card4.appendTo(elemCardsContainer)
+    card5.appendTo(elemCardsContainer)
+    card6.appendTo(elemCardsContainer)
+    card7.appendTo(elemCardsContainer)
+    card8.appendTo(elemCardsContainer)
+    card9.appendTo(elemCardsContainer)
